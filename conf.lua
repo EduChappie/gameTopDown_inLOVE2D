add615405bebd998f0b4f4d89d4cc39dbbf4ae84
EduardoChappie <eduardofabricio2107@gmail.com>
2026-01-16 18:42:16 -0300

    arquivos base, primeiro commit

---
+++ conf.lua
@@ -0,0 +1,9 @@
+function love.conf(t)
+    t.console = true
+    t.version = "11.5"
+    t.window.width = 1320
+    t.window.height = 720
+    t.window.icon = nil
+    t.window.resizable = false
+    t.window.title = "projeto 01"
+end
