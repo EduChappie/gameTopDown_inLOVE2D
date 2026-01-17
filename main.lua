17255a2ad1353141430687c80d079e92aa1b9f7b
EduardoChappie <eduardofabricio2107@gmail.com>
2026-01-17 15:44:33 -0300

    Criação do gerador de blocos ou a parede via classes e objetos, sem colisão no momento

--- main.lua
+++ main.lua
@@ -1,7 +1,12 @@
 require 'characters/player'
+require 'objects/blockGenerator'
 
 function love.load()
     p:load()
+
+    block1 = block:new(1, 1, 1319, 5)
+    block2 = block:new(0, 714, 1319, 5)
+    block3 = block:new(800, 50, 50, 200)
 end
 
 function love.update(dt)
@@ -10,4 +15,8 @@ end
 
 function love.draw()
     p:draw()
+
+    block1:draw()
+    block2:draw()
+    block3:draw()
 end
---
+++ objects/blockGenerator.lua
@@ -0,0 +1,20 @@
+block = {}
+block.__index = block
+
+function block:new(px, py, pw, ph)
+    local self = setmetatable({}, block)
+    self.x = px
+    self.y = py
+    self.w = pw
+    self.h = ph
+    return self
+end
+
+function block:update(dt)
+    -- possível função com colisão
+end
+
+function block:draw() -- printar esse bloco
+    love.graphics.setColor(1, 1, 1)
+    love.graphics.rectangle("line", self.x, self.y, self.w, self.h)
+end
